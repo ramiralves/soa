ba7bd286cabf5b56a1a25b68b5e3ab700822c2b3
ramiralves <ramiralves@gmail.com>
2022-07-13 16:23:09 -0300

    [RAMIR] - CLIENT RESTFUL REFERENTE AO SERVICO "autor-restful-service".

--- autor-restful-client/src/test/java/autor/restful/client/autor/restful/client/AutenticacaoTest.java
+++ autor-restful-client/src/test/java/autor/restful/client/autor/restful/client/AutenticacaoTest.java
@@ -35,7 +35,6 @@ public class AutenticacaoTest {
 		if(!StringUtils.isEmpty(response.getBody())) {
 			retorno = true;
 			System.out.println(String.format("JWT Token: [%s]", response.getBody()));
-				
 		}		
 		assertTrue(retorno);
 		
